8fb8fbf377efc4b8c3f81aa709bb66e43d7adf13
unknown <960350484@kccs.auth>
2023-05-11 11:01:38 +0900

    locations-history

--- src/pages/location-history.js
+++ src/pages/location-history.js
@@ -4,12 +4,37 @@ import { listMetcom3DLocations } from '../graphql/queries';
 import { useParams } from 'react-router-dom';
 
 function LocationHistory () {
-    const { DeviceID } = useParams();
+    const { deviceID } = useParams();
+    const [locations, setLocations] = useState([]);
+
+    useEffect(() => {
+        fetchLocations(deviceID);
+    }, [deviceID]);
+
+    async function fetchLocations(deviceID) {
+        try {
+            const apiData = await API.graphql(graphqlOperation(listMetcom3DLocations, {
+                filter: { DeviceID: { eq: deviceID}},
+                limit: 100
+            }));
+            const locations = apiData.data.listMetcom3DLocations.items;
+            const sortedLocations = locations.sort((a, b) => b.Timestamp - a.Timestamp);
+            setLocations(sortedLocations);
+        } catch (err ) {
+            console.error('Error fetching locations', err);
+        }
+    };  
 
     return (
         <div>
-            <h2>Device Details for ID: {DeviceID}</h2>
-            {/* ここで取得した詳細情報を表示するなど */}
+            <h2>Device Details for ID: {deviceID}</h2>
+            {locations.map(location => (
+                <div key={location.id}>
+                    <p>Timestamp: {location.Timestamp}</p>
+                    <p>Pressure: {location.Pressure}</p>
+                    <p>HAT: {location.Hat}</p>
+                </div>
+            ))}
         </div>
     );    
 }
