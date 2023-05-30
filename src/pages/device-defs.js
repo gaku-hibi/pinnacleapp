ee932054bb7a250b0c04c20f416efc78887627e1
gaku-hibi <gaku-hibi@kccs.co.jp>
2023-05-30 16:47:47 +0900

    devicedefs

--- src/pages/device-defs.js
+++ src/pages/device-defs.js
@@ -24,67 +24,23 @@ const DeviceDefs = () => {
       console.error('Error fetching device IDs', error);
     }
   };
-/*
-  const fetchData = async (deviceId) => {
-    try {
-      let nextToken = null;
-  
-      do {
-        const deviceData = await API.graphql(graphqlOperation(listMetComDeviceDefsByDeviceIDSortedTimestamp, { 
-          DeviceID: deviceId, 
-          nextToken: nextToken,
-        }));
-  
-        if (deviceData.data) {
-          const newData = deviceData.data.listMetComDeviceDefsByDeviceIDSortedTimestamp.items;
-          const mappedData = newData.map(d => ({
-            ...d,
-            Timestamp: d.Timestamp * 1000, // Here, we keep the Timestamp as a Date object
-          }));
 
-          // Add data to the corresponding timestamp object
-          mappedData.forEach(item => {
-            // Check if an object for the timestamp already exists in the data array
-            let timestampObject = data.find(datum => datum.timestamp === item.Timestamp);
-  
-            if (timestampObject) {
-              // Add the new device data to the existing timestamp object
-              timestampObject[deviceId] = item.PressureDef;
-            } else {
-              // Create a new object for the timestamp and add the device data
-              data.push({
-                timestamp: item.Timestamp,
-                [deviceId]: item.PressureDef
-              });
-            }
-          });
-        }
-  
-        nextToken = deviceData.data.listMetComDeviceDefsByDeviceIDSortedTimestamp.nextToken;
-      } while (nextToken);
-    } catch (error) {
-      console.error(`Error fetching device data for ID ${deviceId}`, error);
-    }
-  };
-  */
   const fetchData = async (deviceId) => {
     try {
       let nextToken = null;
       let fetchedData = [];
   
       do {
-        const deviceData = await API.graphql(graphqlOperation(listMetComDeviceDefsByDeviceIDSortedTimestamp, { 
-          DeviceID: deviceId, 
+        const deviceData = await API.graphql(graphqlOperation(listMetComDeviceDefsByDeviceIDSortedTimestamp, {
+          DeviceID: deviceId,
           nextToken: nextToken,
         }));
   
         if (deviceData.data) {
           const newData = deviceData.data.listMetComDeviceDefsByDeviceIDSortedTimestamp.items.map(item => ({
-            deviceId: deviceId,
             timestamp: item.Timestamp * 1000,
             [deviceId]: item.PressureDef // the key of the pressuredef value is now the device id
           }));
-          console.log(newData);
   
           fetchedData = [...fetchedData, ...newData];
         }
@@ -93,16 +49,26 @@ const DeviceDefs = () => {
       } while (nextToken);
   
       setData(prevData => {
-        // Merge the fetched data with the existing data and sort by timestamp
-        const updatedData = [...prevData, ...fetchedData].sort((a, b) => a.timestamp - b.timestamp);
-        return updatedData;
+        let updatedData = [...prevData];
+  
+        fetchedData.forEach(fetchedDatum => {
+          let existingDatum = updatedData.find(datum => datum.timestamp === fetchedDatum.timestamp);
+  
+          if (existingDatum) {
+            existingDatum[deviceId] = fetchedDatum[deviceId];
+          } else {
+            updatedData.push(fetchedDatum);
+          }
+        });
+  
+        return updatedData.sort((a, b) => a.timestamp - b.timestamp);
       });
   
     } catch (error) {
       console.error(`Error fetching device data for ID ${deviceId}`, error);
     }
   };
-  
+
   useEffect(() => {
     fetchAllDeviceIDs();
   }, []); 
@@ -110,7 +76,7 @@ const DeviceDefs = () => {
   const formatXAxis = (tickItem) => {
     // Assume that the tickItem is a timestamp in seconds
     // If it is in milliseconds, you do not need to multiply by 1000
-    const date = new Date(tickItem * 1000);
+    const date = new Date(tickItem);
   
     // Return date in YYYY/MM/DD format
     return `${date.getFullYear()}/${String(date.getMonth() + 1).padStart(2, '0')}/${String(date.getDate()).padStart(2, '0')}`;
